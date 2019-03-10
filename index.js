0532093ac4b0e58289fc780384c33eba6c74b84a
Wighawag <wighawag@gmail.com>
2019-03-10 19:17:28 +0000

    do differences check here, not in rocketh

--- index.js
+++ index.js
@@ -6,31 +6,69 @@ function setup(rocketh, Web3Provided) {
     } else {
         const Web3 = require('web3');
         web3 = new Web3(rocketh.ethereum);
-    }     
+    }
 
-    const deployContractIfNew = async (name, contractName, options, ...args) => {
+    const deploy = async(name, contractName, options, ...args) => {
         const ContractInfo = rocketh.contractInfo(contractName);
-        let contract;
-        await rocketh.unlessAlreadyDeployed(name, ContractInfo.evm.bytecode.object, args, async (registerDeployment) => {
-            const Contract = new web3.eth.Contract(ContractInfo.abi);
-            contract = await Contract.deploy({data:'0x' + ContractInfo.evm.bytecode.object, arguments: args}).send(options);
-            registerDeployment(name, { 
-                contractInfo: ContractInfo, 
-                args, 
-                address: contract.options.address
-            });
+        const Contract = new web3.eth.Contract(ContractInfo.abi);
+        const promiEvent = Contract.deploy({data:'0x' + ContractInfo.evm.bytecode.object, arguments: args}).send(options);
+        let transactionHash;
+        promiEvent.once('transactionHash', (txHash) => {
+            transactionHash = txHash;
+        });
+        const contract = await promiEvent;
+        registerDeployment(name, { 
+            contractInfo: ContractInfo, 
+            address: contract.options.address,
+            transactionHash,
+            args
         });
         return contract;
-    };
-
-    const deployOrGetExistingContract = async (name, contractName, options, ...args) => {
-        let contract = await deployContractIfNew(name, contractName, options, ...args);
-        if(!contract) {
-            const deployment = rocketh.deployment(name);
-            contract = new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);
-        }
-        return contract;
     }
+
+    const deployIfDifferent = async (fieldsToCompare, name, contractName, options, ...args) => {
+        const deployment = rocketh.deployment(name);
+        const transaction = await web3.eth.getTransaction(deployment.transactionHash);
+        
+        const ContractInfo = rocketh.contractInfo(contractName);
+        const Contract = new web3.eth.Contract(ContractInfo.abi);
+
+        const compareOnData = fieldsToCompare.indexOf('data') != -1;
+        const compareOnInput = fieldsToCompare.indexOf('input') != -1;
+
+        let data;
+        if(compareOnData || compareOnInput) {
+            data = Contract.deploy({data:'0x' + ContractInfo.evm.bytecode.object, arguments: args}).encodeABI()
+        }  
+        const newTransaction = {
+            data: compareOnData ? data : undefined,
+            input: compareOnInput ? data : undefined,
+            gas: options.gas,
+            gasPrice: options.gasPrice,
+            value: options.value,
+            from: options.from
+        };
+
+        let differences = false;
+        transaction.data = transaction.input;
+        for(let i = 0; i < fieldsToCompare.length; i++) {
+            const field = fieldsToCompare[i];
+            if(typeof newTransaction[field] == 'undefined') {
+                console.error('field ' + field + ' not specified in new transaction, cant compare');
+                return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);
+            }
+            if(transaction[field] != newTransaction[field]) {
+                differences = true;
+                break;
+            }
+        }
+        if(!differences) {
+            return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);    
+        }
+        console.log('deploying');
+        return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);    
+        //return deploy(name, contractName, options, ...args);
+    };
 
     function getDeployedContract(name) {
         const deployment = rocketh.deployment(name);
@@ -38,9 +76,8 @@ function setup(rocketh, Web3Provided) {
     }
 
     return {
-        deployContractIfNew,
+        deployIfDifferent,
         getDeployedContract,
-        deployOrGetExistingContract,
         web3
     };
 }
