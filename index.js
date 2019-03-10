3bd25b5daa464d1f9cf12f77c8dfef8267189ae9
Wighawag <wighawag@gmail.com>
2019-03-10 19:24:51 +0000

    fixes

--- index.js
+++ index.js
@@ -28,46 +28,46 @@ function setup(rocketh, Web3Provided) {
 
     const deployIfDifferent = async (fieldsToCompare, name, contractName, options, ...args) => {
         const deployment = rocketh.deployment(name);
-        const transaction = await web3.eth.getTransaction(deployment.transactionHash);
+        if(deployment) {
+            const transaction = await web3.eth.getTransaction(deployment.transactionHash);
         
-        const ContractInfo = rocketh.contractInfo(contractName);
-        const Contract = new web3.eth.Contract(ContractInfo.abi);
+            const ContractInfo = rocketh.contractInfo(contractName);
+            const Contract = new web3.eth.Contract(ContractInfo.abi);
 
-        const compareOnData = fieldsToCompare.indexOf('data') != -1;
-        const compareOnInput = fieldsToCompare.indexOf('input') != -1;
+            const compareOnData = fieldsToCompare.indexOf('data') != -1;
+            const compareOnInput = fieldsToCompare.indexOf('input') != -1;
 
-        let data;
-        if(compareOnData || compareOnInput) {
-            data = Contract.deploy({data:'0x' + ContractInfo.evm.bytecode.object, arguments: args}).encodeABI()
-        }  
-        const newTransaction = {
-            data: compareOnData ? data : undefined,
-            input: compareOnInput ? data : undefined,
-            gas: options.gas,
-            gasPrice: options.gasPrice,
-            value: options.value,
-            from: options.from
-        };
+            let data;
+            if(compareOnData || compareOnInput) {
+                data = Contract.deploy({data:'0x' + ContractInfo.evm.bytecode.object, arguments: args}).encodeABI()
+            }  
+            const newTransaction = {
+                data: compareOnData ? data : undefined,
+                input: compareOnInput ? data : undefined,
+                gas: options.gas,
+                gasPrice: options.gasPrice,
+                value: options.value,
+                from: options.from
+            };
 
-        let differences = false;
-        transaction.data = transaction.input;
-        for(let i = 0; i < fieldsToCompare.length; i++) {
-            const field = fieldsToCompare[i];
-            if(typeof newTransaction[field] == 'undefined') {
-                console.error('field ' + field + ' not specified in new transaction, cant compare');
-                return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);
+            let differences = false;
+            transaction.data = transaction.input;
+            for(let i = 0; i < fieldsToCompare.length; i++) {
+                const field = fieldsToCompare[i];
+                if(typeof newTransaction[field] == 'undefined') {
+                    console.error('field ' + field + ' not specified in new transaction, cant compare');
+                    return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);
+                }
+                if(transaction[field] != newTransaction[field]) {
+                    differences = true;
+                    break;
+                }
             }
-            if(transaction[field] != newTransaction[field]) {
-                differences = true;
-                break;
+            if(!differences) {
+                return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);    
             }
         }
-        if(!differences) {
-            return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);    
-        }
-        console.log('deploying');
-        return new web3.eth.Contract(deployment.contractInfo.abi, deployment.address);    
-        //return deploy(name, contractName, options, ...args);
+        return deploy(name, contractName, options, ...args);
     };
 
     function getDeployedContract(name) {
